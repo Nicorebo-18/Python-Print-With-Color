d7958a83268d3091e459ca0174dc68b826d533b1
Nicorebo-18 <Nicorebo18@gmail.com>
2023-09-08 01:01:47 +0200

    Update 8 Bit colors

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
-def Color8Bit(text, style="0", color="37", background_color="37"):
-    # Diccionario de colores en inglés a códigos ANSI
+def Color8Bits(text, foreground="reset", background="reset", styles=[]):
+    
     color_codes = {
-        "black": "30",
-        "red": "31",
-        "green": "32",
-        "yellow": "33",
-        "blue": "34",
-        "magenta": "35",
-        "cyan": "36",
-        "white": "37",
+        "black": 0,
+        "red": 1,
+        "green": 2,
+        "yellow": 3,
+        "blue": 4,
+        "magenta": 5,
+        "cyan": 6,
+        "white": 7,
+        "default": 9,
+        "reset": 0
     }
 
-    # Diccionario de estilos a códigos ANSI
     style_codes = {
-        "bold": "1",
-        "italic": "3",
-        "underline": "4",
-        "blink": "5",
-        "reverse": "7",
-        "reset": "0",
+        "bold": 1,
+        "dim": 2,
+        "italic": 3,
+        "underline": 4,
+        "blink": 5,
+        "reverse": 7,
+        "hidden": 8,
+        "strikethrough": 9
     }
 
-    # Verifica si se proporcionó un estilo y obtiene su código ANSI
-    style_code = style_codes.get(style, "")
+    foreground_code = color_codes.get(foreground.lower(), 0)
+    background_code = color_codes.get(background.lower(), 0)
 
-    # Verifica si se proporcionó un color de texto y obtiene su código ANSI
-    color_code = color_codes.get(color, "")
-    color_code = f"38;5;{color_code}" if color_code else ""
+    style_sequence = ";".join(str(style_codes.get(style.lower(), "")) for style in styles)
+    
+    escape_sequence = f"\x1b[{style_sequence};38;5;{foreground_code}m\x1b[48;5;{background_code}m"
+    reset_sequence = "\x1b[0m\n"
+    colored_text = f"{escape_sequence}{text}{reset_sequence}"
 
-    # Verifica si se proporcionó un color de fondo y obtiene su código ANSI
-    background_code = color_codes.get(background_color, "")
-    background_code = f"48;5;{background_code}" if background_code else ""
-
-    # Combina los códigos de estilo, color de texto y color de fondo
-    code = ";".join(filter(None, [style_code, color_code, background_code]))
-
-    # Aplica la secuencia de escape ANSI y muestra el texto
-    if code:
-        print(f"\033[{code}m{text}\033[0m")
-    else:
-        print(text)
+    print(colored_text, end="")
 
 
 def ColorRGB(text, foreground=(255, 255, 255), background=(0, 0, 0)):
--- test.py
+++ test.py
@@ -1,13 +1,39 @@
-def ColorRGB(text, foreground=(255, 255, 255), background=(0, 0, 0)):
-    fg_r, fg_g, fg_b = foreground
-    bg_r, bg_g, bg_b = background
+def Color8Bits(text, foreground="reset", background="reset", styles=[]):
+    color_codes = {
+        "black": 0,
+        "red": 1,
+        "green": 2,
+        "yellow": 3,
+        "blue": 4,
+        "magenta": 5,
+        "cyan": 6,
+        "white": 7,
+        "default": 9,
+        "reset": 0
+    }
 
-    escape_sequence = f"\033[38;2;{fg_r};{fg_g};{fg_b}m\033[48;2;{bg_r};{bg_g};{bg_b}m"
-    reset_sequence = "\033[0m\n"
+    style_codes = {
+        "bold": 1,
+        "dim": 2,
+        "italic": 3,
+        "underline": 4,
+        "blink": 5,
+        "reverse": 7,
+        "hidden": 8,
+        "strikethrough": 9
+    }
+
+    foreground_code = color_codes.get(foreground.lower(), 0)
+    background_code = color_codes.get(background.lower(), 0)
+
+    style_sequence = ";".join(str(style_codes.get(style.lower(), "")) for style in styles)
+    
+    escape_sequence = f"\x1b[{style_sequence};38;5;{foreground_code}m\x1b[48;5;{background_code}m"
+    reset_sequence = "\x1b[0m\n"
     colored_text = f"{escape_sequence}{text}{reset_sequence}"
 
     print(colored_text, end="")
 
 if __name__ == "__main__":
     # Ejemplo de uso:
-    ColorRGB("Texto con colores RGB")
+    Color8Bits("Texto con colores", foreground="red", styles=["bold", "underline"])
